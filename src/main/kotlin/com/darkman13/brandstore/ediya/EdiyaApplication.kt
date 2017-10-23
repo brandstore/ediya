dedbe955e415616d25557d93280427c7c9b74dec
pierre.c <pierre.c@kakaocorp.com>
2017-10-23 22:45:06 +0900

    first commit

---
+++ src/main/kotlin/com/darkman13/brandstore/ediya/EdiyaApplication.kt
@@ -0,0 +1,11 @@
+package com.darkman13.brandstore.ediya
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class EdiyaApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(EdiyaApplication::class.java, *args)
+}
---
+++ src/test/kotlin/com/darkman13/brandstore/ediya/EdiyaApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.darkman13.brandstore.ediya
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class EdiyaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
